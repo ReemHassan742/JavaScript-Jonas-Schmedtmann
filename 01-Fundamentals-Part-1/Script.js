0bed2b0f6d43cdcb37688ddbfc8a00e9c6f317d7
Reem Hassan <reemhassan742r@gmail.com>
2025-01-19 17:14:04 +0200

    Add files via upload

---
+++ 01-Fundamentals-Part-1/Script.js
@@ -0,0 +1,39 @@
+/*
+-Values and Variables (Assignment)
+
+let js = "amazing";
+if (js === "amazing") alert("Hello world!");
+
+console.log(40 + 33 + 22 + 11 - 99);
+
+let firstName = "Reem"
+let secondName = "Hassan"
+let age = 20
+
+console.log(firstName);
+console.log(secondName);
+console.log(age);
+
+let country = "Egypt";
+let city = "Banha";
+let population = 182.254;
+
+console.log(country);
+console.log(city);
+console.log(population);
+*/
+
+
+
+/* -Data Types (Assignment)
+
+let isIsland = false;
+let population = 182.254;
+let country = "Egypt";
+let language = "Arabic";
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language); 
+*/
